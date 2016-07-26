c4f42165ccea2aa7d6eab1513688e3685d0ea6ad
Gabi Procell <gprocell927@gmail.com>
2016-07-26 14:14:34 -0600

    Added self to merchant

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,14 +2,18 @@ class Merchant
   attr_reader :id,
               :name,
               :creation_date,
-              :updated_date
+              :updated_date,
+              :repo
 
-  def initialize(information)
+  def initialize(information, repo)
     @id = information[0]
     @name = information[1]
     @creation_date = information[2]
     @updated_date = information[3]
+    @repo = repo
   end
+
+
 
 
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -3,34 +3,41 @@ require './lib/merchant'
 
 class TestMerchant < Minitest::Test
   def test_merchant_exists
-    assert_instance_of Merchant, Merchant.new("test")
+    assert_instance_of Merchant, Merchant.new("test", self)
   end
 
   def test_it_has_a_id
-    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"])
+    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"], self)
     id = "12334105"
 
     assert_equal id, merchant.id
   end
 
   def test_it_has_a_name
-    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"])
+    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"], self)
     name = "Shopin1901"
 
     assert_equal name, merchant.name
   end
 
   def test_creation_date
-    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"])
+    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"], self)
     creation = "2010-12-10"
 
     assert_equal creation, merchant.creation_date
   end
 
   def test_it_has_an_updated_date
-    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"])
+    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"], self)
     update = "2011-12-04"
 
     assert_equal update, merchant.updated_date
+  end
+
+  def test_self_can_be_passed_into_merchant
+    merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"], self)
+
+
+    assert_equal self, merchant.repo
   end
 end
