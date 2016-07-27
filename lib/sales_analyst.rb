75c927d275ec49788fad611332364b948cfe31ff
Ryan Workman <rdavid1099@gmail.com>
2016-07-27 17:50:43 -0600

    Sales analyst calculates average.  Still working on std deviation

---
+++ lib/sales_analyst.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class SalesAnalyst
+  attr_reader :sales_engine,
+              :total_num_of_items,
+              :total_num_of_merchants,
+              :num_of_merchants_items
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+    @total_num_of_items = sales_engine.items.all.length
+    @total_num_of_merchants = sales_engine.merchants.all.length
+    @num_of_merchants_items = generate_total_items_of_each_merchant
+  end
+
+  def average_items_per_merchants
+    num_of_merchants_items.reduce(:+) / num_of_merchants_items.length.to_f
+  end
+
+  def average_items_per_merchant_standard_deviation
+    binding.pry
+    Math.sqrt(difference_of_merchant_items_and_average / 2)
+  end
+
+  def difference_of_merchant_items_and_average
+    num_of_merchants_items.reduce(0) do |result, number|
+      result += ((number - average_items_per_merchants)**2)
+      result
+    end
+  end
+
+  def generate_total_items_of_each_merchant
+    sales_engine.merchants.all.map do |merchant|
+      validate_number_of_items(merchant.items)
+    end
+  end
+
+  def validate_number_of_items(items)
+    return items.length unless items.nil?
+    0
+  end
+end
---
+++ test/sales_analyst_test.rb
@@ -0,0 +1,58 @@
+require './test/test_helper'
+require './lib/sales_analyst'
+require './lib/sales_engine'
+
+class TestSalesAnalyst < Minitest::Test
+  def test_sales_analyst_can_receive_sales_engine
+    se = SalesEngine.from_csv({:items => "./data/test_items.csv", :merchants => "./data/test_merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_instance_of SalesAnalyst, sa
+  end
+
+  def test_sales_analyst_knows_total_number_of_items
+    se = SalesEngine.from_csv({:items => "./data/test_items.csv", :merchants => "./data/test_merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_equal true, sa.total_num_of_items > 30
+  end
+
+  def test_sales_analyst_knows_total_number_of_merchants
+    se = SalesEngine.from_csv({:items => "./data/test_items.csv", :merchants => "./data/test_merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_equal true, sa.total_num_of_merchants > 30
+  end
+
+  def test_analyst_returns_zero_if_item_number_is_zero
+    se = SalesEngine.from_csv({:items => "./data/test_items.csv", :merchants => "./data/test_merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_equal 0, sa.validate_number_of_items(nil)
+  end
+
+  def test_analyst_creates_array_of_total_items_by_each_merchant
+    se = SalesEngine.from_csv({:items => "./data/test_items.csv", :merchants => "./data/test_merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_instance_of Array, sa.num_of_merchants_items
+    assert_equal true, sa.num_of_merchants_items.length > 15
+    assert_equal 6, sa.num_of_merchants_items.inject(:+)
+  end
+
+  def test_sales_analyst_calculates_average_amount_of_items_per_merchant
+    se = SalesEngine.from_csv({:items => "./data/test_items.csv", :merchants => "./data/test_merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_instance_of Float, sa.average_items_per_merchants
+    assert_equal true, sa.average_items_per_merchants < 1
+  end
+
+  def test_sales_analyst_calculates_standard_deviation_of_average
+    se = SalesEngine.from_csv({:items => "./data/items.csv", :merchants => "./data/merchants.csv"})
+    sa = SalesAnalyst.new(se)
+
+    assert_instance_of Float, sa.average_items_per_merchant_standard_deviation
+    assert_equal 1, sa.average_items_per_merchant_standard_deviation
+  end
+end
