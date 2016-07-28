62ad57c9907274da1f7724cd839d41918acaf184
Gabi Procell <gprocell927@gmail.com>
2016-07-28 08:59:25 -0600

    working on invoice repository

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,15 +1,16 @@
 class InvoiceRepository
 
-  attr_reader :invoices
+  attr_reader :pathname,
+              :sales_engine,
+              :all
 
-  def initialize(invoices=[])
-    @invoices = invoices
+  def initialize(pathname,sales_engine)
+    @pathname = pathname
+    @sales_engine = sales_engine
+    @all = []
   end
 
-  def all
-    invoices
-  end
-
-  def find_by_id(id)
+  def add_invoice(data)
+    all << Invoice.new(data, self)
   end
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -5,60 +5,33 @@ require './lib/invoice'
 class InvoiceRepositoryTest < Minitest::Test
 
   def test_it_exists
-    ir = InvoiceRepository.new
+    ir = InvoiceRepository.new("test", self)
     assert_instance_of InvoiceRepository, ir
   end
 
+  def test_it_can_receive_pathname_and_self
+    ir = InvoiceRepository.new("path", self)
+    assert_equal "path", ir.pathname
+    assert_equal self, ir.sales_engine
+  end
+
   def test_it_can_return_an_empty_array_when_there_are_no_known_invoices
-    ir = InvoiceRepository.new
+    ir = InvoiceRepository.new("path", self)
     assert_equal [], ir.all
   end
 
-  def test_it_can_return_an_array_of_all_known_invoice_instances
-    invoices = [Invoice.new, Invoice.new]
-    ir       = InvoiceRepository.new(invoices)
+   def test_it_adds_an_instance_of_invoice_to_all
+     ir = InvoiceRepository.new("path", self)
+     invoice_one = {:id => "29",:customer_id => "7",:merchant_id => "12334861",:status => "shipped", :created_at => "2008-09-21", :updated_at => "2010-10-21"}
+     ir.add_invoice(invoice_one)
 
-    assert_equal 2, ir.all.count
-    ir.all.each do |invoice|
-      assert_instance_of Invoice, invoice
-    end
-  end
+     assert_equal 1, ir.all.count
+     assert_instance_of Invoice, ir.all[0]
+   end
 
-  def test_it_returns_nil_for_no_matching_id
-    invoices = [Invoice.new, Invoice.new]
-    ir       = InvoiceRepository.new(invoices)
-    id       = "12345"
+   def test_it_finds_an_instance_of_invoice_with_matching_id
+     ir = InvoiceRepository.new("path", self)
+     invoice_one = {:id => "29",:customer_id => "7",:merchant_id => "12334861",:status => "shipped", :created_at => "2008-09-21", :updated_at => "2010-10-21"}
+     ir.add_invoice(invoice_one)
 
-    assert_equal nil,ir.find_by_id(id)
-  end
-
-  def test_it_returns_an_instance_of_invoice_with_matching_id
-    id       = "12345"
-    invoices = [Invoice.new(id), Invoice.new]
-    ir       = InvoiceRepository.new(invoices)
-
-    assert_equal invoices.first, ir.find_by_id(id)
-  end
-
-  def test_it_returns_a_blank_array_for_customer_id
-    ir       = InvoiceRepository.new(invoices)
-
-  end
-
-  def test_it_returns_one_or_more_matches_with_matching_customer_id
-
-  end
-
-  def test_it_returns_a_blank__merchant_array
-  end
-
-  def test_it_returns_one_or_more_matches_with_matching_merchant_id
-
-  end
-
-  def test_it_returns_an_empty_status_array
-  end
-
-  def test_it_returns_one_or_more_matches_with_matching_status
-  end
 end
