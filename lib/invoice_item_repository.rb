4e5f831ae945898be3917f5822bdeecc464fe927
Gabi Procell <gprocell927@gmail.com>
2016-07-30 16:34:47 -0600

    more work on invoice item repository

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,14 +1,17 @@
+require '../lib/invoice_item'
+
 class InvoiceItemRepository
 
-  attr_reader :all,
-              :sales_engine
+  attr_reader :sales_engine,
+              :all
 
-  def initialize(sales_engine)
+  def initialize(information, sales_engine)
     @sales_engine = sales_engine
     @all = Array.new
   end
 
   def add_data(data)
-    @all << data
+    @all << InvoiceItem.new(data, self)
   end
+
 end
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -1,16 +1,47 @@
 require './test/test_helper'
 require './lib/invoice_item_repository'
-require './lib/sales_engine'
+
 class TestInvoiceItemRepository < Minitest::Test
-  def test_can_return_array_of_all_known_invoice_item_instances
+
+  def test_invoice_item_repository_exists
     iir = InvoiceItemRepository.new("test", self)
-    invoice_item_one = {:id => "1", :item_id => "263519844", :invoice_id => "1", :quantity => "5", :unit_price => "13635", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"}
-    invoice_item_two = {:id => "2", :item_id => "263454779", :invoice_id => "1", :quantity => "9", :unit_price => "23324", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"}
 
-    iir.add_data(invoice_item_one)
-    iir.add_data(invoice_item_two)
-
-    assert_equal 2, iir.all.count
+    assert_instance_of InvoiceItemRepository, iir
   end
+
+  def test_repository_receives_self_from_other_classes
+    iir = InvoiceItemRepository.new("test", self)
+
+    assert_equal self, iir.sales_engine
+  end
+
+  def test_repository_adds_items
+    iir = InvoiceItemRepository.new("test", self)
+    iir.add_data({:id => "1", :item_id => "263519844", :invoice_id => "1", :quantity => "5", :unit_price => "13635", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"})
+
+    assert_instance_of InvoiceItem, iir.all[0]
+    assert_equal 1, iir.all.length
+  end
+
+  def test_invoice_item_repository_can_add_more_than_one_item
+    iir = InvoiceItemRepository.new("test", self)
+    iir.add_data({:id => "1", :item_id => "263519844", :invoice_id => "1", :quantity => "5", :unit_price => "13635", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"})
+    iir.add_data({:id => "2", :item_id => "263454779", :invoice_id => "1", :quantity => "9", :unit_price => "23324", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"})
+
+
+    assert_equal 2, iir.all.length
+  end
+
+
+  # def test_can_return_array_of_all_known_invoice_item_instances
+  #   iir = InvoiceItemRepository.new("test", self)
+  #   invoice_item_one = {:id => "1", :item_id => "263519844", :invoice_id => "1", :quantity => "5", :unit_price => "13635", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"}
+  #   invoice_item_two = {:id => "2", :item_id => "263454779", :invoice_id => "1", :quantity => "9", :unit_price => "23324", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"}
+  #
+  #   iir.add_data(invoice_item_one)
+  #   # iir.add_data(invoice_item_two)
+  #
+  #   assert_equal 2, iir.all.count
+  # end
 
 end
