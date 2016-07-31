c2a303f12c269526c5b1b8141b485cb6e72800c7
Ryan Workman <rdavid1099@gmail.com>
2016-07-31 10:59:01 -0600

    Fixed minor bugs with invoice item and repo

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -14,9 +14,9 @@ class InvoiceItem
 
   def initialize (information, repo)
     @id = information[:id].to_i
-    @item_id = information[:item_id]
-    @invoice_id = information[:invoice_id]
-    @quantity = information[:quantity]
+    @item_id = information[:item_id].to_i
+    @invoice_id = information[:invoice_id].to_i
+    @quantity = information[:quantity].to_i
     @created_at = Time.parse(information[:created_at])
     @updated_at = Time.parse(information[:updated_at])
     @unit_price = BigDecimal.new(information[:unit_price], 4)/100
