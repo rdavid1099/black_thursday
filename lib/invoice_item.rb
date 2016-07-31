432de1303f10f678022bc4807ade9c1eadc50877
Gabi Procell <gprocell927@gmail.com>
2016-07-31 11:31:06 -0600

    made customer

---
+++ lib/invoice_item.rb
@@ -0,0 +1,31 @@
+require 'bigdecimal'
+
+class InvoiceItem
+  attr_reader  :id,
+               :item_id,
+               :invoice_id,
+               :quantity,
+               :unit_price,
+               :created_at,
+               :updated_at,
+               :unit_price,
+               :repo
+
+
+  def initialize (information, repo)
+    @id = information[:id].to_i
+    @item_id = information[:item_id]
+    @invoice_id = information[:invoice_id]
+    @quantity = information[:quantity]
+    @created_at = Time.parse(information[:created_at])
+    @updated_at = Time.parse(information[:updated_at])
+    @unit_price = BigDecimal.new(information[:unit_price], 4)/100
+    @repo = repo
+  end
+
+  def unit_price_to_dollars
+    unit_price.to_f
+  end
+
+
+end
